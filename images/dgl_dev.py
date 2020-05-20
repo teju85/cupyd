616b5cc7dfb3d7d801e149a2a8fce4e636fabcc1
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-05-19 20:23:02 -0700

    added devtools inside dgl image

--- images/dgl_dev.py
+++ images/dgl_dev.py
@@ -2,18 +2,30 @@ from __future__ import absolute_import
 import modules.dev_env
 import modules.conda
 import modules.jupyter
+import modules.cuda
 
 
 def emit(writer, **kwargs):
     if "cudaVersion" not in kwargs:
         raise Exception("'cudaVersion' is mandatory!")
+    cudaVersion = kwargs["cudaVersion"]
     modules.conda.emit(writer)
     writer.condaPackages(["cudatoolkit=$cudaVersion", "dgl-cuda$cudaVersion",
                           "nltk", "notebook", "pytorch", "torchvision"],
                          channels=["pytorch", "dglteam"],
-                         cudaVersion=kwargs["cudaVersion"])
+                         cudaVersion=cudaVersion)
     modules.jupyter.emit(writer, **kwargs)
     modules.dev_env.emit(writer, **kwargs)
+    _, _, versionShort, pkgVersion = modules.cuda.shortVersion(cudaVersion)
+    short = float(versionShort)
+    pkgs = []
+    if short >= 10.0:
+        pkgs += [
+            "cuda-nsight-compute-$pkgVersion",
+            "cuda-nsight-systems-$pkgVersion",
+        ]
+    writer.packages(pkgs, pkgVersion=pkgVersion,
+                    installOpts="--allow-downgrades")
 
 
 def images():
