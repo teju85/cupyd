fa1337156739b479aaab3e02443f49584e213d30
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-05-25 23:35:27 -0700

    new image for cuda development

---
+++ images/cunet_dev.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import
+import modules.dev_env
+import modules.cuda
+
+
+def emit(writer, **kwargs):
+    if "cudaVersion" not in kwargs:
+        raise Exception("'cudaVersion' is mandatory!")
+    cudaVersion = kwargs["cudaVersion"]
+    modules.dev_env.emit(writer, **kwargs)
+    _, _, versionShort, pkgVersion = modules.cuda.shortVersion(cudaVersion)
+    short = float(versionShort)
+    pkgs = []
+    if short >= 10.0:
+        pkgs += [
+            "cuda-nsight-compute-$pkgVersion",
+            "cuda-nsight-systems-$pkgVersion",
+        ]
+    writer.packages(pkgs, pkgVersion=pkgVersion,
+                    installOpts="--allow-downgrades")
+
+
+def images():
+    return {
+        "cunet-dev:1804-102": {
+            "cudaVersion": "10.2",
+            "base": "nvidia/cuda:10.2-cudnn7-devel-ubuntu18.04",
+            "needsContext": True,
+        }
+    }
