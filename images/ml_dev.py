0cbbe3a71f6752dc641bb86fc78afd30f32845f0
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-11-01 18:11:16 -0800

    updated rapids version to the latest branch

--- images/ml_dev.py
+++ images/ml_dev.py
@@ -19,12 +19,12 @@ def emit(writer, **kwargs):
     modules.dev_env.emit(writer, **kwargs)
 
 
-rapidsVersion = "0.16"
+rapidsVersion = "0.17"
 def images():
     imgs = {}
     for osVer in ["18.04"]:
         verStr = osVer.replace(".", "")
-        for cudaVer in ["10.2", "11.0", "11.1"]:
+        for cudaVer in ["10.2", "11.0"]:
             _, _, short, _ = modules.cuda.shortVersion(cudaVer)
             short = short.replace(".", "")
             imgName = "ml-dev:%s-%s" % (verStr, short)
