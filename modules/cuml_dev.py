1b8a3237c2d1bf59cd9f0029e30f6ef8a2c64357
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-11-09 21:06:21 -0800

    merged gnn and ml dev envs

--- modules/cuml_dev.py
+++ modules/cuml_dev.py
@@ -2,16 +2,10 @@ import modules.conda
 import modules.cuda as cuda
 import modules.jupyter
 
-def emit(writer, **kwargs):
-    modules.conda.emit(writer)
-    if "rapidsVersion" not in kwargs:
-        raise Exception("'rapidsVersion' is mandatory!")
-    if "cudaVersion" not in kwargs:
-        raise Exception("'cudaVersion' is mandatory!")
-    _, _, cudaVersionShort, _ = cuda.shortVersion(kwargs["cudaVersion"])
+def ml_env(writer, cudaVersionShort, rapidsVersion):
     short = float(cudaVersionShort)
     repo = "rapidsai"
-    branch = "branch-" + kwargs["rapidsVersion"]
+    branch = "branch-" + rapidsVersion
     writer.emit("""RUN wget "https://raw.githubusercontent.com/$repo/cuml/$branch/conda/environments/cuml_dev_cuda$cudaVersionShort.yml" \\
         -O cuml_dev.yml && \\
     conda env create -n cuml_dev -f cuml_dev.yml && \\
@@ -20,6 +14,27 @@ def emit(writer, **kwargs):
                 repo=repo,
                 branch=branch,
                 cudaVersionShort=cudaVersionShort)
+    writer.emit("""RUN conda install -c conda-forge clang=11.0.0 clang-tools=11.0.0""")
+
+
+def gnn_env(writer):
+    writer.emit("COPY contexts/envs/gnn_dev.yaml /tmp/gnn_dev.yaml")
+    writer.emit("""RUN \\
+    conda env create -n gnn_dev -f /tmp/gnn_dev.yaml && \\
+    rm -f /tmp/gnn_dev.yaml && \\
+    conda clean --yes --all""")
+
+
+def emit(writer, **kwargs):
+    modules.conda.emit(writer)
+    if "rapidsVersion" not in kwargs:
+        raise Exception("'rapidsVersion' is mandatory!")
+    if "cudaVersion" not in kwargs:
+        raise Exception("'cudaVersion' is mandatory!")
+    _, _, cudaVersionShort, _ = cuda.shortVersion(kwargs["cudaVersion"])
+    ml_env(writer, cudaVersionShort, kwargs["rapidsVersion"])
+    gnn_env(writer)
     modules.jupyter.emit(writer, **kwargs)
-    writer.emit("COPY contexts/cuml-dev /cuml-dev")
-    writer.emit("COPY contexts/raft-dev /raft-dev")
+    writer.emit("COPY contexts/envs/cuml-dev /cuml-dev")
+    writer.emit("COPY contexts/envs/raft-dev /raft-dev")
+    writer.emit("COPY contexts/envs/gnn-dev /gnn-dev")
