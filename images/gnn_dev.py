4406f9996292288c06974252fe7d9893bcb0a2ca
Thejaswi Rao <rao.thejaswi@gmail.com>
2022-02-10 01:51:25 -0800

    updated gnn-dev to explicitly include cudnn into conda yaml

--- images/gnn_dev.py
+++ images/gnn_dev.py
@@ -2,22 +2,19 @@ from __future__ import absolute_import
 import modules.dev_env
 import modules.gnn_dev
 import modules.cuda
-import modules.cudnn_dev
+import modules.cuda_dev
 import modules.internal
 
 
 def emit(writer, **kwargs):
     if "cudaVersion" not in kwargs:
         raise Exception("'cudaVersion' is mandatory!")
-    if "cudnnVersion" not in kwargs:
-        raise Exception("'cudnnVersion' is mandatory!")
     if "base" not in kwargs:
         raise Exception("'base' is mandatory!")
     if "rcUrl" not in kwargs:
         kwargs["rcUrl"] = None
-    modules.cudnn_dev.emit(writer, kwargs["cudnnVersion"],
-                           kwargs["cudaVersion"], kwargs["base"],
-                           kwargs["rcUrl"])
+    modules.cuda_dev.emit(writer, kwargs["cudaVersion"], kwargs["base"],
+                          kwargs["rcUrl"])
     modules.gnn_dev.emit(writer, **kwargs)
     modules.dev_env.emit(writer, **kwargs)
 
@@ -27,17 +24,13 @@ def images():
     for osVer in ["20.04"]:
         verStr = osVer.replace(".", "")
         for cudaVer in ["11.2", "11.5"]:
-            for cudnnVer in ["8.2"]:
-                _, _, cu_short, _ = modules.cuda.shortVersion(cudaVer)
-                cu_short = cu_short.replace(".", "")
-                _, _, short = modules.cudnn_dev.shortVersion(cudnnVer)
-                short = short.replace(".", "")
-                imgName = "gnn-dev:%s-%s-%s" % (verStr, cu_short, short)
-                imgs[imgName] = {
-                    "cudaVersion": cudaVer,
-                    "cudnnVersion": cudnnVer,
-                    "base": "ubuntu:%s" % osVer,
-                    "needsContext": True,
-                }
+            _, _, cu_short, _ = modules.cuda.shortVersion(cudaVer)
+            cu_short = cu_short.replace(".", "")
+            imgName = "gnn-dev:%s-%s" % (verStr, cu_short)
+            imgs[imgName] = {
+                "cudaVersion": cudaVer,
+                "base": "ubuntu:%s" % osVer,
+                "needsContext": True,
+            }
     imgs.update(modules.internal.read_rc())
     return imgs
