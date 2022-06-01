f3b669e4b7f7325731873fba845bb321e75641fe
Thejaswi Rao <rao.thejaswi@gmail.com>
2022-06-01 08:09:52 -0700

    do not use mamba as it is causing conflicts during dependency resolution in conda!

--- modules/conda.py
+++ modules/conda.py
@@ -9,4 +9,4 @@ def emit(writer):
     rm /opt/miniconda.sh
 
 ENV PATH /opt/conda/bin:$${PATH}""")
-    writer.condaPackages(["libarchive", "mamba", "python=3.9"], channels=["base", "conda-forge"])
+    writer.condaPackages(["libarchive", "python=3.9"], channels=["base", "conda-forge"])
